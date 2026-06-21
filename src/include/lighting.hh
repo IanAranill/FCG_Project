c61e83637fbf2066e6e3118b55835f67e4ee6aa7
IanAranill <104172584+IanAranill@users.noreply.github.com>
2026-06-21 20:23:22 +0200

    Refactor code structure for improved readability and maintainability

--- src/include/lighting.hh
+++ src/include/lighting.hh
@@ -20,7 +20,7 @@ class Lighting {
 
     Lighting() {
         // Valori iniziali Luce
-        light_position = glm::vec3(0.0f, 1000.0f, 0.0f);
+        light_position = glm::vec3(0.0f, 10.0f, 0.0f);
         light_color = glm::vec3(1.0f, 1.0f, 1.0f);
         light_ambient = glm::vec3(0.2f, 0.2f, 0.2f);
 
